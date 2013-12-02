cc07b07a0013e8951a9992276ad65e3f943ccf08
Martin Warne <martmatwarne@gmail.com>
2013-12-02 23:50:28 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
-raspberrypi-timelapse
-=====================
+#Raspberry Pi Timelapse Camera with Dropbox Sync
 
-Raspberry Pi timelapse software that uses the Raspberry Pi camera and Dropbox
+This program is meant to be loaded onto the RaspberryPi which has the RaspberryPi camera attached to it. It can then be setup to run via a cron job to take pictures as often as you want.
+
+##Setup
+
+The instructions below are written as if you haven't installed anything on your Pi apart from the OS.
+
+####Download
+
+The best way to download the code is through git. To install git do
+
+    sudo apt-get install git
+    
+Then you can download the code by running the following in the directory you want to download into
+
+    git clone git@github.com:martmatwarne/raspberrypi-timelapse.git
+
+####Requirements
+
+The best way to install the requirements is through pip. To get pip if you don't already have it:
+
+    sudo apt-get install python-setuptools
+	sudo easy_install pip
+    
+then do
+
+    pip install -r requirements.pip
+
+####Dropbox program
+
+You'll need to register with Dropbox to get app key and app secret values.
+
+1. Go to https://www.dropbox.com/developers/apps
+2. Select 'Create app'
+3. Select 'Dropbox API app'
+4. Select 'Files and datastores'
+5. Select 'Yes My app only needs access to files it creates.'
+6. Name it something
+7. Save the app
+8. Grab the app key and secret from the app page and enter them into settings.json
+
+####Access Key
+
+To sync with Dropbox you'll need to authenticate the program. This is the horrible bit of the setup and something I want to fix! But for now run the program
+
+    python photo_taker.py
+
+The output will then tell you the url you have to go to. Once you've authorised the app an access token will appear that you can type in. If this is done correctly then the program will be able to upload files to the Apps directory in your Dropbox. The program will then add it to data.json so you don't have to keep entering it.
+
+####Set it up via a cron job
+
+Ideally you want to run this script via cron. A guide to cron can be found here or just by googling
+
+<a href="http://www.thegeekstuff.com/2009/06/15-practical-crontab-examples/">http://www.thegeekstuff.com/2009/06/15-practical-crontab-examples/</a>
+
+##Output
+The program produces two things. It produces a new picture everytime it runs and it regenerates the video to include the new picture.
+
+##Authors
+
+Martin Warne @martmatwarne
+
+##Version
+
+0.1 - Initial release, 02/12/2013
+
+##TODO
+
+* Fix it so when you git clone you don't get conflicts on the JSON files
+* Make Dropbox authentication nicer
+* Add other upload services
+* Try and make the video encode remotely
+* Add more options to configure images and the video
+* Find a solution that isn't everyone creating their own Dropbox apps
+
---
+++ photo_taker.py
@@ -0,0 +1,153 @@
+import dropbox
+import json
+import os
+import time
+
+
+class DropboxUpload(object):
+    """Simple Class to help uploading files to Dropbox"""
+
+    @staticmethod
+    def _read_value(file, json_key):
+        json_file = open("{file}.json".format(file=file), "r")
+        data = json.load(json_file)
+        json_file.close()
+        try:
+            return data[json_key]
+        except KeyError:
+            return False
+
+    @staticmethod
+    def _save_access_token(access_token):
+        # load the json
+        json_file = open("data.json", "r")
+        data = json.load(json_file)
+        json_file.close()
+        # edit the value
+        data['accessToken'] = access_token
+        # save the file
+        json_file = open("data.json", "w+")
+        json_file.write(json.dumps(data))
+        json_file.close()
+        return True
+
+    @classmethod
+    def _get_access_token(cls):
+        """
+        Return the Dropbox access token if we have one or get the user to 
+        generate it
+        """
+
+        # If we already have an access token
+        access_token = cls._read_value('data', 'accessToken')
+        if access_token:
+            return access_token
+        else:
+            app_key = self._read_value('settings', 'appKey')
+            app_secret = self._read_value('settings', 'appSecret')
+            flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, 
+                                                              app_secret)
+            authorize_url = flow.start()
+            print '1. Go to: ' + authorize_url
+            print '2. Click "Allow" (you might have to log in first)'
+            print '3. Copy the authorization code.'
+            code = raw_input("Enter the authorization code here: ").strip()
+            access_token, user_id = flow.finish(code)
+            cls._save_access_token(access_token=access_token)
+            return access_token
+
+    @classmethod
+    def _get_client(cls):
+        """Get the Dropbox client"""
+        access_token = cls._get_access_token()
+        return dropbox.client.DropboxClient(access_token)
+
+    @classmethod
+    def upload_file(cls, file_path, filename):
+        """Upload a file to the server"""
+        f = open("{file_path}".format(file_path=file_path), 'r')
+        client = cls._get_client()
+        response = client.put_file(filename, f, overwrite=True)
+        print "uploaded:", response
+        return filename
+
+
+class RaspberryPiCamera:
+    """Class to interact through the RaspberryPi camera"""
+    
+    def __init__(self):
+        self.folder = 'images'
+        self.last_filename = ''
+
+    def _create_name(self):
+        """Create a filename based off the counter"""
+
+        # load the json
+        json_file = open("data.json", "r")
+        data = json.load(json_file)
+        json_file.close()
+        # edit the value
+        counter = data['counter'] + 1
+        data['counter'] = counter
+        # save the file
+        json_file = open("data.json", "w+")
+        json_file.write(json.dumps(data))
+        json_file.close()
+
+        return 'image-%04d.jpg' % (counter,)
+
+    def get_picture_location(self, filename=False):
+        """
+        By default returns the path to the last file. If a filename is passed in
+        then it will return the location that file will be save to.
+        """
+        if not filename:
+            filename = self.last_filename
+        return "{folder}/{filename}".format(folder=self.folder, 
+                                            filename=filename)
+
+    def take_picture(self):
+        """Take a picture with the camera"""
+        filename = self._create_name()
+        file_location = self.get_picture_location(filename=filename)
+        os.system("raspistill -o {path_to_image}".format(path_to_image=file_location))
+        self.last_filename = filename
+        return True
+
+
+class VideoGenerator(object):
+    """Takes a series of images and produces a time lapse video out of them"""
+    file_path = 'images'
+    filename = 'timelapse.mp4'
+
+    @staticmethod
+    def create_video():
+        """Use anconv to create the timelapse."""
+        # TODO this is really slow. Need to find a way to do it remotely on a
+        # more powerful box
+        os.system("avconv -y -i images/image-%04d.jpg -r 1 video/timelapse.mp4")
+        return True
+
+
+def main():
+    """
+    Takes a picture, uploads it to Dropbox.
+    Makes a timelapse video, uploads it to Dropbox.
+    """
+    # Take a photo
+    camera = RaspberryPiCamera()
+    picture = camera.take_picture()
+
+    # Upload the image
+    DropboxUpload.upload_file(file_path=camera.get_picture_location(), 
+                              filename=camera.last_filename)
+
+    # Make the video
+    video = VideoGenerator.create_video()
+
+    # Upload the video
+    DropboxUpload.upload_file(file_path='video/timelapse.mp4',
+                              filename='timelapse.mp4')
+
+if __name__ == "__main__":
+    main()
